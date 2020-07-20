c7e7f7aa14d687e90f7a717b1347a1496591ea97
chenxiuting <chen.xiuting@xyb2b.com>
2020-07-20 18:24:18 +0800

    1

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 // webhooks
 app.use('/speeder', speederRouter);
 
