0d25a79bc6a95a90ba0a834d90b97dfe6e4cfae2
chenxiuting <chen.xiuting@xyb2b.com>
2020-07-20 18:39:44 +0800

    1

--- routes/deploy.sh
+++ routes/deploy.sh
@@ -5,8 +5,8 @@ WEB_USERGROUP='root'
 echo "Start deployment $WEB_PATH"
 cd $WEB_PATH
 echo "pulling source code..."
-git reset --hard origin/master
-git clean -f
+### git reset --hard origin/master
+### git clean -f
 git pull
 ### git checkout master
 echo "changing permissions..."
