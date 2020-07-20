35c40b98c46e93f947d25f9361b7fa67e6458fa2
chenxiuting <chen.xiuting@xyb2b.com>
2020-07-20 18:05:45 +0800

    fix: cc

--- routes/speeder.js
+++ routes/speeder.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 var createHandler = require('github-webhook-handler');
 // rewrite path '/'
 var handler = createHandler({ path: '/', secret: '402821051@qq.com' });
 
-router.post('/', function (req, res, next) {
+router.all('/', function (req, res, next) {
   handler(req, res, function (err) {
+    pullRepo()
     res.statusCode = 404;
     res.end('no such location');
   });
@@ -17,12 +19,28 @@ handler.on('error', function (err) {
 });
 
 handler.on('push', function (event) {
-  // todo 
+  pullRepo()
   console.log(
     'Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref
   );
 });
+
+function pullRepo() {
+  const { exec } = require('child_process');
+  // todo care about pm2 restart child_process
+  exec(
+    `sh "${path.posix.join(__dirname, './deploy.sh')} "${__dirname}"`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      }
+    }
+  );
+}
 
 module.exports = router;
