39255a75cb68e1d33dcca610140b303755301ea2
cjz9032 <402821051@qq.com>
2020-07-21 01:45:41 +0800

    chore: upload

--- public/rubbish/apend-all.js
+++ public/rubbish/apend-all.js
@@ -1,23 +1,19 @@
-
-var loadStyle = function(url) {
-    var link = document.createElement('link');
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = url;
-    var head = document.getElementsByTagName("head")[0];
-    head.appendChild(link);
-  };
-  // css加载
-  function loadJS( url, callback ){
+var loadStyle = function (url) {
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = url;
+  var head = document.getElementsByTagName('head')[0];
+  head.appendChild(link);
+};
+// css加载
+function loadJS(url, callback) {
   var script = document.createElement('script'),
-      fn = callback || function(){};
+    fn = callback || function () {};
   script.type = 'text/javascript';
   script.src = url;
   document.getElementsByTagName('head')[0].appendChild(script);
 }
-
-
- window.stop();
 
 if (window.plus) {
   plusReady();
@@ -32,22 +28,29 @@ function plusReady() {
     function () {
       webview.canBack(function (e) {
         console.log(e);
+		
         if (e.canBack) {
           webview.back();
         } else {
-          webview.close();
+          'iOS' == plus.os.name
+            ? plus.nativeUI.confirm(
+                '确认不搞了？',
+                function (e) {
+                  if (e.index > 0) {
+                    webview.close();
+                  }
+                },
+                'alijiao',
+                ['cancel', 'sure']
+              )
+            : confirm('确认不搞了？') && webview.close();
         }
       });
     },
     false
   );
-
-  // append lib + js(bee) + css in outer
-  window.webpackJsonp = [];
-  window.www = webview
-		webview.setCssFile('_www/assets/dist/all.css');
-      webview.appendJsFile('_www/assets/dist/all.js');
-      setTimeout(()=>{
-        loadStyle('https://goodmujian.xyz/vjs.css')
-      }, 100)
+  
+  
+   
+  window.webview = webview;
 }
