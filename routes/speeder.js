fd3f7b4cd409eae233bfacf0452b87c0e607fc02
chenxiuting <chen.xiuting@xyb2b.com>
2020-07-20 18:14:05 +0800

    r

--- routes/speeder.js
+++ routes/speeder.js
@@ -8,7 +8,7 @@ var handler = createHandler({ path: '/', secret: '402821051@qq.com' });
 
 router.all('/', function (req, res, next) {
   handler(req, res, function (err) {
-    pullRepo()
+    pullRepo();
     res.statusCode = 404;
     res.end('no such location');
   });
@@ -19,7 +19,7 @@ handler.on('error', function (err) {
 });
 
 handler.on('push', function (event) {
-  pullRepo()
+  pullRepo();
   console.log(
     'Received a push event for %s to %s',
     event.payload.repository.name,
@@ -30,17 +30,19 @@ handler.on('push', function (event) {
 function pullRepo() {
   const { exec } = require('child_process');
   // todo care about pm2 restart child_process
-  exec(
-    `sh "${path.posix.join(__dirname, './deploy.sh')}" "${path.posix.join(__dirname, '../')}"`,
-    (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      }
+  const run = `sh "${path.posix.join(
+    __dirname,
+    './deploy.sh'
+  )}" "${path.posix.join(__dirname, '../')}"`;
+  console.log(run);
+  exec(run, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
     }
-  );
+  });
 }
 
 module.exports = router;
